b6c431d3c53981f9dbf2f564893f36a2e908df94
nonlocalStream <lcq9051@berkeley.edu>
2016-02-11 23:47:01 -0800

    part8 and competition

--- svg/my_examples/star.py
+++ svg/my_examples/star.py
@@ -29,17 +29,51 @@ def polygon(points, fill="#ffffff", opacity=1):
 def transform(trans, svg):
     return '\n<g transform="'+trans+'">\n'+svg+"\n</g>\n"
 
-def star(x, y, r, c="#cccc00"):
+def colortri(points, colors):
+    s = '<colortri stroke-width="0" points="'
+    for x,y in points:
+        s = s+str(x)+" "+str(y)+" "
+    s = s+'" colors="'
+    for c in colors:
+        for e in c:
+            s = s + str(e) + " "
+    s = s+'"/>\n'
+    return s
+
+
+def circle(x, y, r, c="#cccc00"):
     k = 20
     p = 360.0/k
     points = []
-    for i in range(20):
+    for i in range(k):
         deg = math.radians(p*i)
         points.append((math.cos(deg)*r, math.sin(deg)*r))
     return transform("translate("+str(x)+" "+str(y)+")", polygon(points, c))
     # return '<g transform="translate('+str(x)+" "+str(y)+')">' + polygon(points, "#cccc00")+ "</g>"
 
-def cloud(X, Y, R, k, star_r, base_c, darkness=1):
+def star(x, y, r, c="#cccc00"):
+    k = 50
+    p = 360.0/k
+    points = []
+    rgb = hex_to_rgb(c)
+    rgba = (rgb[0]/255., rgb[1]/255., rgb[2]/255., 0.8)
+    rgba_trans = (0, 0, 0, 0)
+    s=""
+    for i in range(k):
+        deg1 = math.radians(p*i)
+        deg2 = math.radians(p*(i+1))
+        p1 = (math.cos(deg1)*r, math.sin(deg1)*r)
+        p2 = (math.cos(deg2)*r, math.sin(deg2)*r)
+        s = s+colortri([p1,p2,(0,0)],[rgba_trans, rgba_trans, rgba])
+        # points.append((math.cos(deg)*r, math.sin(deg)*r))
+    # for i in range(len(points)):
+    #     p1 = points[i]
+    #     p2 = points[(i+1)%len(points)]
+    #     s = s+colortri([p1,p2,(0,0)],[rgba_trans, rgba_trans, rgba])
+    return transform("translate("+str(x)+" "+str(y)+")", s)
+
+
+def cloud(X, Y, R, k, star_r, base_c, darkness=1, f=star):
     s = ""
     base_rgb = hex_to_rgb(base_c)
     hsv = rgb_to_hsv(base_rgb[0]/255.0,base_rgb[1]/255.0,base_rgb[2]/255.0)
@@ -51,7 +85,7 @@ def cloud(X, Y, R, k, star_r, base_c, darkness=1):
         deg = random.random()*360
         x = math.cos(deg)*r
         y = math.sin(deg)*r
-        s = s+star(X+x, Y+y, random.random()*star_r, c)
+        s = s+f(X+x, Y+y, random.random()*star_r, c)
     return s
 
 def spiral(spread, l, base_c):
@@ -62,7 +96,7 @@ def spiral(spread, l, base_c):
         r = i*spread
         deg = math.radians(p*i)
         # s = s + star(math.cos(deg)*r, math.sin(deg)*r,1)
-        s = s + cloud(math.cos(deg)*r, math.sin(deg)*r,r/5, 10, 5, base_c, 0.5)
+        s = s + cloud(math.cos(deg)*r, math.sin(deg)*r,r/1.7, 20, 3, base_c, 1, circle)
     return s
 
     
@@ -77,9 +111,14 @@ doc = """<?xml version="1.0" encoding="utf-8"?>
 w = 500
 h = 500
 doc = doc + polygon([(0,0),(w,0),(w,h),(0,h)],"#000000", 1)
+# doc = doc+ transform("translate("+str(w/2)+" "+str(h/2)+")",star(0, 0, 100))+ "\n</svg>"
 # doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+")",star(10, 10, 10))+ "\n</svg>"
-doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+")",cloud(0,0,300,2000, 10, "#00cccc",1))
-doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+") scale(1 0.6)",spiral(4, 6, "#ffff00"))+"\n</svg>"
+doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+")",cloud(0,0,400,300, 50, "#00cccc",1))
+# doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+")",cloud(0,0,600,600, 5, "#ee77ee",1))+"\n</svg>"
+doc = doc+transform("rotate(-30 "+str(w/2)+" "+str(h/2)+")", transform("translate("+str(w/2)+" "+str(h/2)+") scale(1 0.5)",spiral(3, 4, "ee7777")))
+# doc = doc+transform("rotate(-30 "+str(w/2)+" "+str(h/2)+")", transform("translate("+str(w/2)+" "+str(h/2)+") scale(0.8 0.4)",spiral(3, 3, "ff9999")))
+doc = doc+transform("rotate(-30 "+str(w/2)+" "+str(h/2)+")", transform("translate("+str(w/2)+" "+str(h/2)+") scale(0.8 0.4)",spiral(3, 2, "ffff99")))
+doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+")",cloud(0,0,200,1500, 1.5, "#ffffff",1, circle))+"\n</svg>"
 # doc = doc+transform("translate("+str(w/2)+" "+str(h/2)+") scale(-1 0.5)",spiral(4, 6, "ff99ff"))+ "\n</svg>"
 f= open('star.svg','w')
 f.write(doc)
